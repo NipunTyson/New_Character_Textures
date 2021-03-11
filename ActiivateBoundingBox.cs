75329bdf12a3ab33fea8d2e4efe6dacd97aed3a3
Nipun Dumpala <nipun.dumpala@gmail.com>
2021-03-11 06:39:23 +0530

    change01

---
+++ ActiivateBoundingBox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ActiivateBoundingBox : MonoBehaviour {
+
+    void Update() {
+            StartCoroutine(DelayBounds());
+    }
+
+
+
+    IEnumerator DelayBounds() {
+        yield return new WaitForEndOfFrame();
+        foreach (BoundingBox bounds in FindObjectsOfType<BoundingBox>()) {
+            bounds.UpdateBounds();
+        }
+    }
+}
